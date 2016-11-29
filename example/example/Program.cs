98ba13ba08330ab332e890c478f22c91ef0df5b8
VeGoRUS <gucvit@yandex.ru>
2016-11-29 03:18:51 +0300

    first commit

---
+++ example/example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,sum;
+            Console.Clear();
+            Console.WriteLine();
+            a = 3;
+            b = 4;
+            sum = a + b;
+            Console.Write(sum);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
